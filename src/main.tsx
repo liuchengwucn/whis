6574cf45d288370a5e566c8d4cf193dd8a56f74b
Liu Chengwu <liuchengwu@pku.edu.cn>
2025-01-26 12:38:15 +0800

    Add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <FluentProvider theme={webLightTheme}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </FluentProvider>
   </React.StrictMode>
 );
