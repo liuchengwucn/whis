411eef9e4714d70142249c00dd23e80580ae6172
Liu Chengwu <liuchengwu@pku.edu.cn>
2025-02-08 21:53:40 +0800

    Show toast on I/O completion

--- src/editor/components/open-button.tsx
+++ src/editor/components/open-button.tsx
@@ -1,22 +1,34 @@
 import { open } from "@tauri-apps/plugin-dialog";
 import { useEditorStore, useFilePathStore } from "../../lib/stores";
-import { Button } from "@fluentui/react-components";
+import {
+  Button,
+  Toast,
+  ToastTitle,
+  useToastController,
+} from "@fluentui/react-components";
 import { ComponentProps } from "react";
 import { readTextFile } from "@tauri-apps/plugin-fs";
 import { parse } from "ass-compiler";
 
-async function openVideo() {
-  const updateVideoPath = useFilePathStore.getState().updateVideoPath;
-  const result = await open({
-    title: "Select a video file",
-    filters: [{ name: "Video Files", extensions: ["mp4"] }],
-  });
-  if (result) {
-    updateVideoPath(result);
-  }
-}
-
 export function OpenVideoButton({ ...props }: ComponentProps<typeof Button>) {
+  const { dispatchToast } = useToastController();
+  async function openVideo() {
+    const updateVideoPath = useFilePathStore.getState().updateVideoPath;
+    const result = await open({
+      title: "Select a video file",
+      filters: [{ name: "Video Files", extensions: ["mp4"] }],
+    });
+    if (!result) return;
+
+    updateVideoPath(result);
+    dispatchToast(
+      <Toast>
+        <ToastTitle>Video Opened Successfully</ToastTitle>
+      </Toast>,
+      { intent: "success" }
+    );
+  }
+
   return (
     <Button onClick={() => openVideo()} {...props}>
       Open Video
@@ -24,31 +36,40 @@ export function OpenVideoButton({ ...props }: ComponentProps<typeof Button>) {
   );
 }
 
-async function openSubtitle() {
-  const updateSubtitlePath = useFilePathStore.getState().updateSubtitlePath;
-
-  const result = await open({
-    title: "Select a subtitle file",
-    filters: [{ name: "Subtitle Files", extensions: ["ass"] }],
-  });
-  if (!result) return;
-
-  updateSubtitlePath(result);
-  const text = await readTextFile(result);
-  const parsedAss = parse(text);
-  useEditorStore.setState({ parsedAss });
-  if (parsedAss.events.dialogue.length !== 0) {
-    useEditorStore.setState({
-      currentLine: 0,
-      transcription: "",
-      translation: "",
-    });
-  }
-}
-
 export function OpenSubtitleButton({
   ...props
 }: ComponentProps<typeof Button>) {
+  const { dispatchToast } = useToastController();
+
+  async function openSubtitle() {
+    const updateSubtitlePath = useFilePathStore.getState().updateSubtitlePath;
+
+    const result = await open({
+      title: "Select a subtitle file",
+      filters: [{ name: "Subtitle Files", extensions: ["ass"] }],
+    });
+    if (!result) return;
+
+    updateSubtitlePath(result);
+    const text = await readTextFile(result);
+    const parsedAss = parse(text);
+    useEditorStore.setState({ parsedAss });
+    if (parsedAss.events.dialogue.length !== 0) {
+      useEditorStore.setState({
+        currentLine: 0,
+        transcription: "",
+        translation: "",
+      });
+    }
+
+    dispatchToast(
+      <Toast>
+        <ToastTitle>Subtitle Opened Successfully</ToastTitle>
+      </Toast>,
+      { intent: "success" }
+    );
+  }
+
   return (
     <Button onClick={() => openSubtitle()} {...props}>
       Open Subtitle
--- src/editor/components/save-button.tsx
+++ src/editor/components/save-button.tsx
@@ -1,19 +1,33 @@
-import { Button } from "@fluentui/react-components";
+import {
+  Button,
+  Toast,
+  ToastTitle,
+  useToastController,
+} from "@fluentui/react-components";
 import { stringify } from "ass-compiler";
 import { ComponentProps } from "react";
 import { useEditorStore, useFilePathStore } from "../../lib/stores";
 import { writeTextFile } from "@tauri-apps/plugin-fs";
 
-async function saveSubtitle() {
-  const parsedAss = useEditorStore.getState().parsedAss;
-  if (!parsedAss) return;
-  const subtitlePath = useFilePathStore.getState().subtitlePath;
-
-  const text = stringify(parsedAss);
-  await writeTextFile(subtitlePath, text);
-}
-
 function SaveButton({ ...props }: ComponentProps<typeof Button>) {
+  const { dispatchToast } = useToastController();
+
+  async function saveSubtitle() {
+    const parsedAss = useEditorStore.getState().parsedAss;
+    if (!parsedAss) return;
+    const subtitlePath = useFilePathStore.getState().subtitlePath;
+
+    const text = stringify(parsedAss);
+    await writeTextFile(subtitlePath, text);
+
+    dispatchToast(
+      <Toast>
+        <ToastTitle>Subtitle Saved Successfully</ToastTitle>
+      </Toast>,
+      { intent: "success" }
+    );
+  }
+
   return (
     <Button onClick={() => saveSubtitle()} {...props}>
       Save
